d4475704a9cc0f814bc673b754dbbdc8118b46aa
Evgenya-odyvanchik <Eva.new@inbox.ru>
2026-04-07 13:24:49 +0300

    Домашка 2

---
+++ домашка/фкпфукфу.py
@@ -0,0 +1,6 @@
+lst = ['grape', 'peach', 'pear', 'tangerine', 'banana', 'apple']
+
+fruit_1 = lst[0]
+fruit_6 = lst[-1]
+
+print(f'Первый вкусный фрукт: {fruit_1} и последний вкусный фрукт: {fruit_6}')
