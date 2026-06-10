89e15e4c8769a38a216f128e3d28c4d2d2fb1b86
Evgenya-odyvanchik <Eva.new@inbox.ru>
2026-06-10 10:28:07 +0300

    Практика до 4

--- practice/3_ООП в Python_Practic_1/user.py
+++ practice/3_ООП в Python_Practic_1/user.py
@@ -1,23 +1,30 @@
+from card import Card
+
 class User:
     age = 0
 
-    def __init__(self, name):
+    def __init__(self, name: str):
         print('Я создался')
         self.username = name
 
-    def sayName(self):
+    def sayName(self) -> None:
+        '''Печатает имя пользователя'''
         print('Меня зовут ' , self.username)
 
-    def sayAge(self):
+    def sayAge(self) -> None:
+        '''Печатает возраст пользователя'''
         print(self.age)
 
-    def setAge(self, newAge):
+    def setAge(self, newAge: int) -> None:
+        '''Перепечатывает имя пользователя'''
         self.age = newAge
 
-    def addCard(self, card):
+    def addCard(self, card: Card) -> None:
+        '''Добавляет пользавателю карту'''
         self.card = card
 
-    def getCard(self):
+    def getCard(self) -> Card:
+        '''Возвращает пользавателю карту'''
         return self.card
 
 
